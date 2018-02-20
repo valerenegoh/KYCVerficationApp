1310caa8fec9da4069c3ec63dc1bac955462c039
WeirdVal <valerene_goh@mymail.sutd.edu.sg>
2018-02-21 01:03:54 +0800

    Updated

--- app/src/main/java/com/example/liyang/remotekyc/MainActivity.java
+++ app/src/main/java/com/example/liyang/remotekyc/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.liyang.remotekyc;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +15,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,36 +45,43 @@ public class MainActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(getApplicationContext(),verification.class));
         }
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String getemail = email.getText().toString().trim();
-                String getpassword = password.getText().toString().trim();
-                callsignin(getemail,getpassword);
-            }
-        });
+        try {
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String getemail = email.getText().toString().trim();
+                    String getpassword = password.getText().toString().trim();
+                    callsignin(getemail, getpassword);
+                }
+            });
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
 
     }
 
     private void callsignin(String email, String password){
-        mAuth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("Testing","Sign In Successful:" + task.isSuccessful());
+        try {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Testing", "Sign In Successful:" + task.isSuccessful());
 
-                        //If Sign In failed, displays a message to the user
-                        if(!task.isSuccessful()){
-                            Log.v("Testing","SignInwithEmail:Failed",task.getException());
-                            Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_SHORT).show();
+                            //If Sign In failed, displays a message to the user
+                            if (!task.isSuccessful()) {
+                                Log.v("Testing", "SignInwithEmail:Failed", task.getException());
+                                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent verification = new Intent(MainActivity.this, com.example.liyang.remotekyc.verification.class);
+                                finish();
+                                startActivity(verification);
+                            }
                         }
-                        else{
-                            Intent verification = new Intent(MainActivity.this, com.example.liyang.remotekyc.verification.class);
-                            finish();
-                            startActivity(verification);
-                        }
-                    }
-                });
+                    });
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
