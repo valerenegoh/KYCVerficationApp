79d0e884d530a8a8f59ecb3bf51db2fbf743c87b
WeirdVal <valerene_goh@mymail.sutd.edu.sg>
2018-02-20 14:21:09 +0800

    update
    added fingerprint button and new fingerprint layout

--- app/src/main/java/com/example/liyang/remotekyc/signup.java
+++ app/src/main/java/com/example/liyang/remotekyc/signup.java
@@ -20,7 +20,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Matcher;
